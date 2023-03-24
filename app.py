c107716320e3e3589ee11d031a38358cabb5f04f
Huiping Guo <huiping192@gmail.com>
2023-03-24 21:42:24 +0900

    fix

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 import ffmpeg
 import matplotlib.pyplot as plt
 import uuid
+import subprocess
 
 app = Flask(__name__)
 
@@ -16,40 +17,14 @@ def home():
     return render_template('index.html', graph_filename=None)
 
 def generate_bitrate_graph(video_url):
-    video_bitrates, audio_bitrates = extract_bitrates(video_url)
+    # Create the bitrate graph
+    graph_filename = f"static/bitrate_graph_{uuid.uuid4().hex}.png"
 
-    plt.figure()
-    plt.plot(video_bitrates, label='Video Bitrate (kbps)')
-    plt.plot(audio_bitrates, label='Audio Bitrate (kbps)')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Bitrate (kbps)')
-    plt.legend()
-    plt.title('Video and Audio Bitrate Graph')
-
-    unique_id = uuid.uuid4()
-    graph_filename = f'static/bitrate_graph_{unique_id}.png'
-    plt.savefig(graph_filename)
+    # Run the plotbitrate command with the desired options
+    command = f"plotbitrate -o {graph_filename} {video_url}"
+    subprocess.run(command, shell=True, check=True)
 
     return graph_filename
-
-def extract_bitrates(video_url):
-    probe = ffmpeg.probe(video_url)
-    video_bitrates = []
-    audio_bitrates = []
-
-    if 'packets' in probe:
-        for packet in probe['packets']:
-            if packet['codec_type'] == 'video':
-                video_bitrates.append(int(packet['bit_rate']) / 1000)
-            elif packet['codec_type'] == 'audio':
-                audio_bitrates.append(int(packet['bit_rate']) / 1000)
-    else:
-        video_stream = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
-        audio_stream = next(stream for stream in probe['streams'] if stream['codec_type'] == 'audio')
-        video_bitrates = [int(video_stream.get('bit_rate', 0)) / 1000] * int(float(video_stream['duration']))
-        audio_bitrates = [int(audio_stream['bit_rate']) / 1000] * int(float(audio_stream['duration']))
-
-    return video_bitrates, audio_bitrates
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
