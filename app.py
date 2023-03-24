fb9bb6b48e90c503a64246e8548129ce11e09a97
Huiping Guo <huiping192@gmail.com>
2023-03-24 21:24:01 +0900

    fix 2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tempfile
 from flask import Flask, render_template, request
 import ffmpeg
 import matplotlib.pyplot as plt
+import uuid
 
 app = Flask(__name__)
 
@@ -10,39 +11,43 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         video_url = request.form['video_url']
-        generate_bitrate_graph(video_url)
-        return render_template('index.html')
-    return render_template('index.html')
+        graph_filename = generate_bitrate_graph(video_url)
+        return render_template('index.html', graph_filename=graph_filename)
+    return render_template('index.html', graph_filename=None)
 
 def generate_bitrate_graph(video_url):
-    # Extract the video and audio bitrates
     video_bitrates, audio_bitrates = extract_bitrates(video_url)
 
-    # Generate the bitrate graph
-    fig, ax = plt.subplots()
-    ax.plot(video_bitrates, label='Video')
-    ax.plot(audio_bitrates, label='Audio')
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Bitrate (kbps)')
-    ax.legend()
+    plt.figure()
+    plt.plot(video_bitrates, label='Video Bitrate (kbps)')
+    plt.plot(audio_bitrates, label='Audio Bitrate (kbps)')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Bitrate (kbps)')
+    plt.legend()
+    plt.title('Video and Audio Bitrate Graph')
 
-    # Save the graph to a temporary file
-    with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as f:
-        plt.savefig(f.name)
-        graph_path = f.name
+    unique_id = uuid.uuid4()
+    graph_filename = f'static/bitrate_graph_{unique_id}.png'
+    plt.savefig(graph_filename)
 
-    return graph_path
+    return graph_filename
 
 def extract_bitrates(video_url):
     probe = ffmpeg.probe(video_url)
     video_bitrates = []
     audio_bitrates = []
 
-    for packet in probe['packets']:
-        if packet['codec_type'] == 'video':
-            video_bitrates.append(int(packet['bit_rate']) / 1000)
-        elif packet['codec_type'] == 'audio':
-            audio_bitrates.append(int(packet['bit_rate']) / 1000)
+    if 'packets' in probe:
+        for packet in probe['packets']:
+            if packet['codec_type'] == 'video':
+                video_bitrates.append(int(packet['bit_rate']) / 1000)
+            elif packet['codec_type'] == 'audio':
+                audio_bitrates.append(int(packet['bit_rate']) / 1000)
+    else:
+        video_stream = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
+        audio_stream = next(stream for stream in probe['streams'] if stream['codec_type'] == 'audio')
+        video_bitrates = [int(video_stream['bit_rate']) / 1000] * int(float(video_stream['duration']))
+        audio_bitrates = [int(audio_stream['bit_rate']) / 1000] * int(float(audio_stream['duration']))
 
     return video_bitrates, audio_bitrates
 
