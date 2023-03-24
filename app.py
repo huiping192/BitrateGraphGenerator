fe36572ab01f9030e8326717b9e631188b38171e
Huiping Guo <huiping192@gmail.com>
2023-03-24 21:48:10 +0900

    add audio graph

--- app.py
+++ app.py
@@ -12,17 +12,23 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         video_url = request.form['video_url']
-        graph_filename = generate_bitrate_graph(video_url)
-        return render_template('index.html', graph_filename=graph_filename)
-    return render_template('index.html', graph_filename=None)
+        video_graph_filename, audio_graph_filename = generate_bitrate_graph(video_url)
+        return render_template('index.html', video_graph_filename=video_graph_filename, audio_graph_filename=audio_graph_filename)
+    return render_template('index.html', video_graph_filename=None, audio_graph_filename=None)
 
 def generate_bitrate_graph(video_url):
-    # Create the bitrate graph
-    graph_filename = f"static/bitrate_graph_{uuid.uuid4().hex}.png"
+    # Create the bitrate graphs
+    video_graph_filename = f"static/video_bitrate_graph_{uuid.uuid4().hex}.png"
+    audio_graph_filename = f"static/audio_bitrate_graph_{uuid.uuid4().hex}.png"
 
-    # Run the plotbitrate command with the desired options
-    command = f"plotbitrate -o {graph_filename} {video_url}"
-    subprocess.run(command, shell=True, check=True)
+    # Run the plotbitrate command with the desired options for video and audio bitrate graphs
+    video_command = f"plotbitrate -o {video_graph_filename} {video_url}"
+    subprocess.run(video_command, shell=True, check=True)
+
+    audio_command = f"plotbitrate -o {audio_graph_filename} -s audio {video_url}"
+    subprocess.run(audio_command, shell=True, check=True)
+
+    return video_graph_filename, audio_graph_filename
 
     return graph_filename
 
